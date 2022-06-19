8cb521dc4818c7f1fd3b6ba34d3cfc0db4f54e4a
seaZonal2001 <kausiksurjit@gmail.com>
2022-06-19 21:11:17 +0530

    Finshing homePage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+
 import './App.css';
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Toaster position='top-center' toastOptions={{
+          success : {
+            theme : {
+              primary: '#4aed88'
+            }
+          }
+        }}></Toaster>
+      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/editor/:roomId' element={<EditorPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
